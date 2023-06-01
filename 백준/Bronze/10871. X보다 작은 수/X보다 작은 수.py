9fa2f1151dd48b30080f5a0eb7c474c444a76fce
nyondooev <125379499+nyondooev@users.noreply.github.com>
2023-06-01 14:04:54 +0900

    [Bronze V] Title: X보다 작은 수, Time: 48 ms, Memory: 32276 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.py
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.py
@@ -1,9 +1,6 @@
 n, x = map(int, input().split())
 num_list = list(map(int, input().split()))
-ans_list = []
 
-for i in num_list:
-    if i < x:
-        ans_list.append(i)
-    
-print(*ans_list)
+for i in range(n):
+    if num_list[i] < x:
+        print(num_list[i], end=' ')        
